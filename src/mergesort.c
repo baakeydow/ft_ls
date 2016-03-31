71e818b08cbfa28009bc89b5171aadf8e9e59dcc
Bilal NDAO <bndao@student.42.fr>
2016-03-31 16:05:43 +0200

    ok

--- src/mergesort.c
+++ src/mergesort.c
@@ -41,6 +41,25 @@ void				part(t_l *head, t_l **front, t_l **back)
 	}
 }
 
+static int datecmp (long long int a, long long int b)
+{
+	if (a <= b)
+		return (1);
+	else if (a > b)
+		return (-1);
+	return (0);
+}
+
+static int decide_sort(t_l *a, t_l *b, t_opt *o)
+{
+	if (o->t)
+	{
+		return (datecmp(a->s.st_mtime, b->s.st_mtime));
+	}
+	else
+		return (ft_strcmp(a->arg, b->arg));
+}
+
 t_l				*merge_lists(t_l *a, t_l *b, t_opt *o)
 {
 	t_l *merge_it;
@@ -50,7 +69,7 @@ t_l				*merge_lists(t_l *a, t_l *b, t_opt *o)
 		return (b);
 	else if (!b)
 		return (a);
-	if (o->r ? ft_strcmp(a->arg, b->arg) > 0 : ft_strcmp(a->arg, b->arg) < 0)
+	if (o->r ? decide_sort(a, b, o) > 0 : decide_sort(a, b, o) < 0)
 	{
 		merge_it = a;
 		merge_it->next = merge_lists(a->next, b, o);
